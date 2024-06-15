41f6f7662d5ee2dac00ca23653771efb071c0815
babsman123 <babspropertyconsult@gmail.com>
2024-06-15 04:45:31 +0100

    another commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -2,6 +2,7 @@ const menuOpen = document.querySelector(".navigation__menu-open");
 const menuClose = document.querySelector(".navigation__menu-close");
 
 const openTextBox = document.querySelector(".header__text-box");
+const sliderBox = document.querySelectorAll(".radio__crew-group");
 console.log(menuOpen);
 
 menuOpen.addEventListener("click", () => {
@@ -17,3 +18,26 @@ menuClose.addEventListener("click", () => {
   menuClose.style.display = "none";
   menuOpen.style.display = "block";
 });
+
+console.log(sliderBox);
+// sliderBox[0].addEventListener("click", () => {
+//   console.log("Helo");
+// });
+
+for (let i = 0; i < sliderBox.length; i++) {
+  sliderBox[0].addEventListener("click", () => {
+    console.log("Helo");
+  });
+
+  sliderBox[1].addEventListener("click", () => {
+    console.log("my life");
+  });
+
+  sliderBox[2].addEventListener("click", () => {
+    console.log("come here");
+  });
+
+  sliderBox[3].addEventListener("click", () => {
+    console.log("green");
+  });
+}
